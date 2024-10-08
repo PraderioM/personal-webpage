4be8692d2d32290775435fa70aff90a057ddec30
PraderioM <PraderioM@Gmail.com>
2024-10-08 10:49:58 +0100

    Now arrows change page if page is too small.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,8 @@ import {faCaretLeft, faCaretRight} from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  homeSelected = true;
-  jobsSelected = false;
+  homeSelected = false; //TODO change back.
+  jobsSelected = true; //TODO change back.
   publicationsSelected = false;
   talksSelected = false;
   teachingSelected = false;
@@ -58,21 +58,34 @@ export class AppComponent {
     this.otherSelected = true;
   }
 
-  getLeftArrowClass(): object {
+  getLeftArrowOver616Class(): object {
     const arrowClass: any = getArrowClass();
     arrowClass['hidden-header'] = this.startingIndex === 0;
-    arrowClass['left-arrow'] = this.startingIndex > 0;
+    arrowClass['left-arrow-over-616'] = this.startingIndex > 0;
     return arrowClass;
   }
 
-  getRightArrowClass(): object {
+  getLeftArrowUnder616Class(): object {
     const arrowClass: any = getArrowClass();
-    arrowClass['right-arrow-number-0'] = this.startingIndex === 0;
-    arrowClass['right-arrow-number-1'] = this.startingIndex === 1;
-    arrowClass['right-arrow-number-2'] = this.startingIndex === 2;
-    arrowClass['right-arrow-number-3'] = this.startingIndex === 3;
-    arrowClass['right-arrow-number-4'] = this.startingIndex === 4;
-    arrowClass['right-arrow-number-5'] = this.startingIndex === 5;
+    arrowClass['hidden-header'] = this.startingIndex === 0;
+    arrowClass['left-arrow-under-616'] = this.startingIndex > 0;
+    return arrowClass;
+  }
+
+  getRightArrowOver616Class(): object {
+    const arrowClass: any = getArrowClass();
+    arrowClass['right-arrow-number-0-over-616'] = this.startingIndex === 0;
+    arrowClass['right-arrow-number-1-over-616'] = this.startingIndex === 1;
+    arrowClass['right-arrow-number-2-over-616'] = this.startingIndex === 2;
+    arrowClass['right-arrow-number-3-over-616'] = this.startingIndex === 3;
+    arrowClass['hidden-header'] = this.startingIndex > 3;
+    return arrowClass;
+  }
+
+  getRightArrowUnder616Class(): object {
+    const arrowClass: any = getArrowClass();
+    arrowClass['hidden-header'] = this.startingIndex === 5;
+    arrowClass['right-arrow-under-616'] = this.startingIndex < 5;
     return arrowClass;
   }
 
@@ -112,7 +125,28 @@ export class AppComponent {
     return headerClass;
   }
 
-  moveStartIndex(step: number) {
+  moveStartIndex(step: number): void {
     this.startingIndex = Math.max(0, Math.min(5, this.startingIndex + step));
+  }
+
+  selectStartingIndex(): void {
+    if (this.startingIndex == 0) {
+      this.selectHome();
+    } else if (this.startingIndex == 1) {
+      this.selectJobs();
+    } else if (this.startingIndex == 2) {
+      this.selectPublications();
+    } else if (this.startingIndex == 3) {
+      this.selectTalks();
+    } else if (this.startingIndex == 4) {
+      this.selectTeaching();
+    } else if (this.startingIndex == 5) {
+      this.selectOther();
+    }
+  }
+
+  changeSelectedPage(step: number): void {
+    this.moveStartIndex(step);
+    this.selectStartingIndex();
   }
 }
